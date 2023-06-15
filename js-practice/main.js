80ddaab49bfbbe4f7f1909498991afc46d13a105
Junior Ugarte <jr.ugartee@gmail.com>
2023-06-15 13:49:27 -0600

    products showed on the main

--- js-practice/main.js
+++ js-practice/main.js
@@ -4,15 +4,17 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const iconMenuMobile = document.querySelector(".icon-menu-mobile");
 const myOrder = document.querySelector(".my-order");
 const shoppingCart = document.querySelector(".shopping-cart");
+const productContainer = document.querySelector(".product-container");
 
 menuEmail.addEventListener("click", changeMenu);
+iconMenuMobile.addEventListener("click", changeMenuMobile);
+shoppingCart.addEventListener("click", changeShoppingCart);
 
 function changeMenu() {
   // Lo que hace esta funcion es sacar o ponerle(toggle) la clase(classList) que le indiquemos:
   desktopMenu.classList.toggle("inactive");
   myOrder.classList.add("inactive");
 }
-iconMenuMobile.addEventListener("click", changeMenuMobile);
 
 function changeMenuMobile() {
   // Lo que hace esta funcion es sacar o ponerle(toggle) la clase(classList) que le indiquemos:
@@ -20,11 +22,69 @@ function changeMenuMobile() {
   myOrder.classList.add("inactive");
 }
 
-shoppingCart.addEventListener("click", changeShoppingCart);
-
 function changeShoppingCart() {
   // Lo que hace esta funcion es sacar o ponerle(toggle) la clase(classList) que le indiquemos:
   myOrder.classList.toggle("inactive");
   desktopMenu.classList.add("inactive");
   mobileMenu.classList.add("inactive");
 }
+// en esta parte, vamos a maquetar los objetos desde JS:
+// Empezamos creando un array en donde le vamos a dar las caracteristicas de los objetos:
+const productList = [];
+productList.push({
+  name: "CRF",
+  price: 2000,
+  img: "./assets/imgs/1.jpg",
+});
+productList.push({
+  name: "CBR",
+  price: 7600,
+  img: "./assets/imgs/2.jpg",
+});
+productList.push({
+  name: "R1",
+  price: 5000,
+  img: "./assets/imgs/3.jpg",
+});
+productList.push({
+  name: "Otra",
+  price: 3000,
+  img: "./assets/imgs/4.jpg",
+});
+function showCards() {
+  // Aca, por cada producto que haya en el array productList, va a crearlos:
+  for (product of productList) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    productContainer.appendChild(productCard);
+
+    const productImg = document.createElement("img");
+    productImg.classList.add("card-image");
+    productImg.setAttribute("src", product.img);
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+    productInfoDiv.append(productPrice, productName);
+
+    const productFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute(
+      "src",
+      "./assets/Platzi_YardSale_Icons/bt_add_to_cart.svg"
+    );
+    productFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productFigure);
+
+    productCard.append(productImg, productInfo);
+
+    productContainer.appendChild(productCard);
+    console.log(product);
+  }
+}
+showCards();
