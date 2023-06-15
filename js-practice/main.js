fceb0332306ed6bd4315dfb8e2e062f96fb0e84a
Junior Ugarte <jr.ugartee@gmail.com>
2023-06-15 17:39:13 -0600

    added interaction

--- js-practice/main.js
+++ js-practice/main.js
@@ -5,21 +5,27 @@ const iconMenuMobile = document.querySelector(".icon-menu-mobile");
 const myOrder = document.querySelector(".my-order");
 const shoppingCart = document.querySelector(".shopping-cart");
 const productContainer = document.querySelector(".product-container");
+const productAside = document.querySelector(".product-aside");
+const productAsideIcon = document.querySelector(".product-close");
 
 menuEmail.addEventListener("click", changeMenu);
 iconMenuMobile.addEventListener("click", changeMenuMobile);
 shoppingCart.addEventListener("click", changeShoppingCart);
+productAsideIcon.addEventListener("click", quitAside);
 
 function changeMenu() {
   // Lo que hace esta funcion es sacar o ponerle(toggle) la clase(classList) que le indiquemos:
   desktopMenu.classList.toggle("inactive");
   myOrder.classList.add("inactive");
+  // productAside.classList.toggle("inactive");
 }
 
 function changeMenuMobile() {
   // Lo que hace esta funcion es sacar o ponerle(toggle) la clase(classList) que le indiquemos:
   mobileMenu.classList.toggle("inactive");
   myOrder.classList.add("inactive");
+  productContainer.classList.add("inactive");
+  productAside.classList.add("inactive");
 }
 
 function changeShoppingCart() {
@@ -27,6 +33,15 @@ function changeShoppingCart() {
   myOrder.classList.toggle("inactive");
   desktopMenu.classList.add("inactive");
   mobileMenu.classList.add("inactive");
+  productAside.classList.add("inactive");
+}
+function showAside() {
+  console.log("funcionando");
+  productAside.classList.remove("inactive");
+}
+function quitAside() {
+  console.log("funcionando");
+  productAside.classList.add("inactive");
 }
 // en esta parte, vamos a maquetar los objetos desde JS:
 // Empezamos creando un array en donde le vamos a dar las caracteristicas de los objetos:
@@ -51,40 +66,37 @@ productList.push({
   price: 3000,
   img: "./assets/imgs/4.jpg",
 });
-function showCards() {
-  // Aca, por cada producto que haya en el array productList, va a crearlos:
-  for (product of productList) {
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card");
-    productContainer.appendChild(productCard);
+// Aca, por cada producto que haya en el array productList, va a crearlos:
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+  productContainer.appendChild(productCard);
 
-    const productImg = document.createElement("img");
-    productImg.classList.add("card-image");
-    productImg.setAttribute("src", product.img);
-    const productInfo = document.createElement("div");
-    productInfo.classList.add("product-info");
+  const productImg = document.createElement("img");
+  productImg.classList.add("card-image");
+  productImg.setAttribute("src", product.img);
+  productImg.addEventListener("click", showAside);
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
 
-    const productInfoDiv = document.createElement("div");
-    const productPrice = document.createElement("p");
-    productPrice.innerText = "$" + product.price;
-    const productName = document.createElement("p");
-    productName.innerText = product.name;
-    productInfoDiv.append(productPrice, productName);
+  const productInfoDiv = document.createElement("div");
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+  productInfoDiv.append(productPrice, productName);
 
-    const productFigure = document.createElement("figure");
-    const productImgCart = document.createElement("img");
-    productImgCart.setAttribute(
-      "src",
-      "./assets/Platzi_YardSale_Icons/bt_add_to_cart.svg"
-    );
-    productFigure.appendChild(productImgCart);
+  const productFigure = document.createElement("figure");
+  const productImgCart = document.createElement("img");
+  productImgCart.setAttribute(
+    "src",
+    "./assets/Platzi_YardSale_Icons/bt_add_to_cart.svg"
+  );
+  productFigure.appendChild(productImgCart);
 
-    productInfo.append(productInfoDiv, productFigure);
+  productInfo.append(productInfoDiv, productFigure);
 
-    productCard.append(productImg, productInfo);
+  productCard.append(productImg, productInfo);
 
-    productContainer.appendChild(productCard);
-    console.log(product);
-  }
+  productContainer.appendChild(productCard);
 }
-showCards();
